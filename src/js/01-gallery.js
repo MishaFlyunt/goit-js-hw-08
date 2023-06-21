bad4c0f12179c0ed98602ef6c4c1f8f59b21a60a
Misha Flyunt <98420437+MishaFlyunt@users.noreply.github.com>
2023-06-21 17:12:58 +0300

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,13 +1,14 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
-const galleryContainer = document.querySelector('.gallery');
-const itemsMarkup = createGalleryItemsMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 
-// rendered items
+const gallery = document.querySelector('.gallery');
+const cardsMarkup = createGalleryItemsMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', cardsMarkup);
+
 function createGalleryItemsMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
@@ -24,10 +25,8 @@ function createGalleryItemsMarkup(items) {
     .join('');
 }
 
-// use library SimpleLightbox
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionPosition: 'bottom',
   captionDelay: 250,
 });
-// console.log(galleryItems);
